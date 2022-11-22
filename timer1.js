edbf3f88e00555670e45eb115815750a28ae97f1
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-22 15:03:21 -0700

    created loop to write a beep to the console at intervals set by the user. if no input given, no beeps, if input is not a number, skipped, if input is less than 0, skips

--- timer1.js
+++ timer1.js
@@ -4,11 +4,20 @@
 
 let input = process.argv;
 input.splice(0,2);
-console.log(input);
-process.stdout.write('\x07'); // causes it to "write" a beeping sound (node system beep)
+//console.log(input);
+if (input.length !== 0) {
+  for (let i in input) {
+    if (input[i] >= 0 && !isNaN(input[i])) {
+      setTimeout(() => {
+        //process.stdout.write(i);
+        process.stdout.write('\x07'); // causes it to "write" a beeping sound (node system beep)
+      }, input[i] * 1000);
+    }
+  }
+}
 
 
-// EDGE CASES 
+// EDGE CASES
 // No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
 // An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
 // An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
