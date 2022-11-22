7a2ea4d4a8547c89cb9ac735b951e08e657b452b
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-22 14:48:07 -0700

    get input from user

---
+++ timer1.js
@@ -0,0 +1,14 @@
+// timer will beep after a specified amount of time has passed (in seconds).
+
+// the user can specify an unlimited number of alarms using command line arguments e.g. node timer1.js 10 3 5 15 9
+
+let input = process.argv;
+input.splice(0,2);
+console.log(input);
+process.stdout.write('\x07'); // causes it to "write" a beeping sound (node system beep)
+
+
+// EDGE CASES 
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
