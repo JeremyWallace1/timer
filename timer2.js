4bb7e5acad8f2b82a8cd7e08ac7f9edb693b0d81
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 09:37:38 -0700

    cleaned up code and removed rl.close() that was preventing it from staying in the keypress mode; also got rid of checking for NaN as it was unnecessary.

--- timer2.js
+++ timer2.js
@@ -1,6 +1,3 @@
-//replace process.argv with user input
-//let input = process.argv;
-//input.splice(0,2);
 const readline = require('readline');
 
 readline.emitKeypressEvents(process.stdin);
@@ -11,32 +8,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-//rl.question("Please set a timer between 1 and 9 seconds: ", (timerSet) => {
-//user can press b and it should beep right away
-let keepRunning = true;
-
-const setATimer = () => {
-  process.stdout.write("Please set a timer between 1 and 9 seconds: ");
-  process.stdin.on("keypress", (str, key) => {
-    if (key.name == 'b') {
-    process.stdout.write(`\x07\n`); //immediately returns a beep
-    // setATimer();
-    } else if (key.ctrl && key.name === 'c') {
-      process.stdout.write(`\nThanks for using me, ciao!\n`);
-      rl.close();
-    } else if (!isNaN(key.name)) {
-      console.log(`\nSetting timer for ${key.name} seconds...`);
-      setTimeout(() => {
-        //process.stdout.write(i);
-        process.stdout.write('\x07'); // causes it to "write" a beeping sound (node system beep)
-      }, key.name * 1000);
-      // setATimer();
-    } else if (isNaN(key.name)) {
-      process.stdout.write(`\nThat's not a number...\n`);
-      // setATimer();
-    }
-    rl.close()
-  })
-};
-
-setATimer();
+process.stdin.on("keypress", (str, key) => {
+  // console.log("Please set a timer between 1 and 9 seconds: ");
+  if (key.name === 'b') {
+    process.stdout.write(`\r \r\x07`); 
+  }
+  if (key.ctrl && key.name === 'c') {
+    console.log(`Thanks for using me, ciao!`);
+    rl.close();
+  }
+  if (!isNaN(key.name)) {
+    console.log(`\rSetting timer for ${key.name} seconds...`);
+    setTimeout(() => {
+      process.stdout.write('\r\x07'); 
+    }, key.name * 1000);
+  }
+});
